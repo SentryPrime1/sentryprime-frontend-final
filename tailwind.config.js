3b950d4bedf15f02e3f0cc2d8c284c995ab8cf3d
SentryPrime1 <gabeguglietti@gmail.com>
2025-08-12 17:04:02 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,36 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./index.html", // Scans the main HTML file
-    "./src/**/*.{js,ts,jsx,tsx}", // Scans all component files in the src folder
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")], // Keep the animation plugin
+  plugins: [require("tailwindcss-animate")],
 }
